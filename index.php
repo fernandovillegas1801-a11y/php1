0e071ce322c550100521d3172726b1fe1059c39b
fernandovillegas1801-a11y <fernandovillegas1801@gmail.com>
2026-06-15 12:42:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,39 +12,39 @@ if (!$input) {
     exit;
 }
 else{
-    echo json_encode([
-    "estado" => "ok"
-]);
-}
-
-$host = getenv('MYSQLHOST');
-$port = getenv('MYSQLPORT');
-$dbname = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-
-$nombre = trim($input['nombre'] ?? '');
-$puntaje = intval($input['intentos'] ?? 0);
-$fecha = trim($input['fecha_hora'] ?? '');
-
-try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",$user,$password);
-
-    $sql = "INSERT INTO records (nombre, puntaje, fecha) VALUES (?, ?, ?)";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([ $nombre, $puntaje, $fecha ]);
-} 
+    $host = getenv('MYSQLHOST');
+    $port = getenv('MYSQLPORT');
+    $dbname = getenv('MYSQLDATABASE');
+    $user = getenv('MYSQLUSER');
+    $password = getenv('MYSQLPASSWORD');
     
-catch (PDOException $e) {
-
-    error_log("Error BD: " . $e->getMessage());
-    http_response_code(500);
-
-    echo json_encode([
-        "estado" => "error",
-        "mensaje" => $e->getMessage()
-    ]);
+    $data = json_decode($input, true);
+    
+    $nombre = trim($data['nombre']);
+    $puntaje = intval($data['intentos']);
+    $fecha = trim($data['fecha_hora']);
+    
+    try {
+        $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",$user,$password);
+    
+        $sql = "INSERT INTO records (nombre, puntaje, fecha) VALUES (?, ?, ?)";
+    
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([ $nombre, $puntaje, $fecha ]);
+        
+        echo json_encode(["estado" => "ok"]);
+    } 
+        
+    catch (PDOException $e) {
+    
+        error_log("Error BD: " . $e->getMessage());
+        http_response_code(500);
+    
+        echo json_encode([
+            "estado" => "error",
+            "mensaje" => $e->getMessage()
+        ]);
+    }
 }
 
 ?>
